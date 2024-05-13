a5fcec37129de464dd20ebdde9c765e1f2872a27
Shubhanshu Sharma <shubhanshu.sharma20@gmail.com>
2024-05-14 00:50:00 +0530

    Add files via upload

--- Javascript_projects/Counter/counter_js.js
+++ Javascript_projects/Counter/counter_js.js
@@ -10,20 +10,33 @@ function onClickReset(e) {
     console.log("Reset Button clicked");
     counter_digital.textContent=0;
     counter_val = 0;
+    setColor(counter_digital,counter_digital.textContent);
 }
 
 function onClickIncr(e) {
     e.preventDefault();
     console.log("Increase Button clicked");
     counter_digital.textContent = ++counter_val;
+    setColor(counter_digital,counter_digital.textContent);
 }
 
 function onClickDecr(e) {
     e.preventDefault();
     console.log("Decrease Button clicked");
     counter_digital.textContent = --counter_val;
+    setColor(counter_digital,counter_digital.textContent);
 }
 
+function setColor(element,number){
+    if(number > 0) {
+        element.style.color = 'green';
+    }
+    else if (number < 0) {
+        element.style.color = 'red';
+    } else {
+        element.style.color = 'black';
+    }
+}
 
 reset_button.addEventListener('click', onClickReset);
 increase_button.addEventListener('click', onClickIncr);
